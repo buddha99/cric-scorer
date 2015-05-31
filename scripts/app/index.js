a6e8a20fd431806d6f9276da5f7e6830725af8ec
Sid <buddha99@gmail.com>
2015-05-30 21:29:21 -0400

    Live-scoring-UI first cut

--- scripts/app/index.js
+++ scripts/app/index.js
@@ -1,13 +1,13 @@
 var app = angular.module('cric-scorer', ['ui.bootstrap', 'ngRoute']);
 
-app.config(function($routeProvider){
+app.config(function ($routeProvider) {
     $routeProvider.when('/', {
         controller: 'main',
         templateUrl: 'views/mainContent.html'
     });
 });
 
-app.controller('main', function($scope, $routeParams){
+app.controller('main', function ($scope, $routeParams) {
     $scope.matchStates = ['Scheduled', 'InProgess', 'Break', 'Stumps', 'Abandoned', 'Over', 'TBD'];
     $scope.activeplayers = {};
     $scope.activeplayers.striker = "Subir";
@@ -26,11 +26,16 @@ app.controller('main', function($scope, $routeParams){
     $scope.extras.totalLegByes = 6;
     
     $scope.ballInProgress = 3;
-    $scope.runNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    $scope.runNumbers = [5, 6, 4, 3, 2, 1, 0];
     $scope.ballModifiers = ['Wide', 'No-Ball', 'Dead'];
-    $scope.wicketTypes = ['BLD', 'Caught', 'Caught-C/O', 'Stmpd', 'R/O', 'H/W'];
+    $scope.wicketTypes = ['Bld', 'St', 'H/W', 'CT', 'CT-C/o', 'R/O-strk', 'R/O-nstrk'];
     
-    $scope.ballUpdate = function(run, modifier, wicket){
-        alert("Run: " + run + "\nModifier: " + modifier + "\nWicket: " + wicket);
-    }
+    $scope.radio = {};
+    $scope.radio.runsRadio = null
+    $scope.radio.ballModifierRadio = null;
+    $scope.radio.wktModifierRadio = null;
+    
+    $scope.ballUpdate = function () {
+        alert("Run: " + this.radio.runsRadio + "\nModifier: " + this.radio.ballModifierRadio + "\nWicket: " + this.radio.wktModifierRadio);
+    };
 });
